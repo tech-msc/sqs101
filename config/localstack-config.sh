99d24c51a690965881e81f0c0df6f526fd05d572
tech-msc <23535116+tech-msc@users.noreply.github.com>
2024-04-24 20:00:10 -0300

    upload files

--- config/localstack-config.sh
+++ config/localstack-config.sh
@@ -6,11 +6,8 @@ echo "configuring localstack"
 #export AWS_SECRET_ACCESS_KEY="test"
 #export AWS_DEFAULT_REGION="us-east-1"
 
-#Estrutura de mensageria de solicitações de emissão
-#awslocal sns create-topic --name sns-topic-example
-awslocal sqs create-queue --queue-name queue-example
-
+awslocal sqs create-queue --queue-name hello-world-queue
 
 # sends example message
-#awslocal sqs send-message --queue-url http://127.0.0.1:4566/000000000000/queue-example --message-body "{'message': 'test'}"
+#awslocal sqs send-message --queue-url http://127.0.0.1:4566/000000000000/hello-world-queue --message-body '{"message": "test"}'
 
